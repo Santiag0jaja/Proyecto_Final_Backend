2bc54efb6a5bd330c0ea7895853a27bd8a5f6c4d
Santiag0jaja <santiagodiazeo@gmail.com>
2025-02-18 21:38:46 -0500

    main changes

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-""" Main code del proyecto """
+""" Main code | Proyecto """
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return "Welcome to the FastAPI application!"
+
